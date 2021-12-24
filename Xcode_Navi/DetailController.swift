35fa8ab7e56a46327737fd18ca2324062ca437ec
lawn <user.lawn@gmail.com>
2021-12-24 17:47:52 +0900

    UI Navi 사용을 학습 unrecognized selector sent to instance 문제를 해결하지 못했음 차후 해결해야함

---
+++ Xcode_Navi/DetailController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailController.swift
+//  Xcode_Navi
+//
+//  Created by 이준영 on 2021/12/24.
+//
+
+import UIKit
+
+class DetailController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+   
+    }
+}
--- Xcode_Navi/ViewController.swift
+++ Xcode_Navi/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func Click_Move(_ sender: Any) {
+       
+        if let controller = self.storyboard?.instantiateViewController(withIdentifier: "DetailController"){
+            self.navigationController?.pushViewController(controller, animated: true)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
