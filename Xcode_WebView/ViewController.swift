a6a0a2bea8ec1c97b7313ac9e6bfda6615c3d077
lawn <user.lawn@gmail.com>
2021-12-25 14:19:03 +0900

    Xcode에서 WebView를 띄우는 법을 학습했습니다.

--- Xcode_WebView/ViewController.swift
+++ Xcode_WebView/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var WebViewMain: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //1. url string
+        //2. url > request
+        //3. req > load
+        
+        let urlString = "https://www.google.com"
+        if let url = URL(string: urlString) {
+            //?는 값이 들어있을수도 있다는 뜻
+            //unwraping 과정이다.
+            let urlReq = URLRequest(url : url)
+            WebViewMain.load(urlReq)
+            
+        }
+        
     }
 
 
