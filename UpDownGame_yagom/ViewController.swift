52932adf3bf85103bbf2cd66b2a74948b302f6fa
lawn <user.lawn@gmail.com>
2022-01-02 21:15:29 +0900

    yagom님의 앱 만들기를 학습중입니다.

--- UpDownGame_yagom/ViewController.swift
+++ UpDownGame_yagom/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func slideerValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
+        print(slider.value)
+    }
 }
 
