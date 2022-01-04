62521048cc114e0e0609f3b00c276cd10fca58af
lawn <user.lawn@gmail.com>
2022-01-04 22:32:14 +0900

    auto layout 기능을 학습했습니다.

--- UpDownGame_yagom/ViewController.swift
+++ UpDownGame_yagom/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        slider.setThumbImage(Image Literal, for: .normal)
     }
 
     @IBAction func slideerValueChanged(_ sender: UISlider) {
